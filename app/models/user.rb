fcb515fc6484ed66c8b31c5ecb6b34bc37dcf581
chamal madusanke <aquachamal@gmail.com>
2017-11-13 13:46:37 +0530

    modify avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,7 +49,7 @@ def production?
 end
 
 
-has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#", :large => "600x600" }, :default_url => "/images/:style/missing.png"
 validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 
